f79d77d953629c8fc27d941a5ec515ef0a8bc623
cloud1992 <arturo@halleyscore.com>
2023-04-30 22:47:27 -0300

    server path fix

--- style_transfer_app/server.py
+++ style_transfer_app/server.py
@@ -1,12 +1,15 @@
-import io
+# import io
 import logging
-import os
 
-import numpy as np
-from common.utils import apply_style_transfer
-from PIL import Image
+# from common.utils import apply_style_transfer
+# from PIL import Image
 from sanic import Sanic, response
 from sanic.exceptions import HTTPException
+
+# import os
+
+# import numpy as np
+
 
 logging.basicConfig(level=logging.INFO)
 app = Sanic(__name__)
@@ -24,40 +27,40 @@ async def handle_request(request):
 
 
 # Función para procesar la imagen y aplicar el estilo
-@app.post("/process")
-async def process(request):
-    # Obtener las imágenes del formulario
-    content_image = request.files.get("content_image")
-    style_image = request.files.get("style_image")
+# @app.post("/process")
+# async def process(request):
+#     # Obtener las imágenes del formulario
+#     content_image = request.files.get("content_image")
+#     style_image = request.files.get("style_image")
 
-    # content_image_name = content_image.name
-    # style_image_name = style_image.name
+#     # content_image_name = content_image.name
+#     # style_image_name = style_image.name
 
-    content_image = content_image.body
-    style_image = style_image.body
+#     content_image = content_image.body
+#     style_image = style_image.body
 
-    # Abrir la imagen usando PIL
-    pil_style_image = Image.open(io.BytesIO(style_image))
-    pil_content_image = Image.open(io.BytesIO(content_image))
+#     # Abrir la imagen usando PIL
+#     pil_style_image = Image.open(io.BytesIO(style_image))
+#     pil_content_image = Image.open(io.BytesIO(content_image))
 
-    # Convertir la imagen PIL a un array de NumPy
-    np_image = np.array(pil_style_image)
-    np_content_image = np.array(pil_content_image)
-    logging.info(f"Content image type: {type(np_image)}")
-    logging.info(f"Content image type: {type(np_content_image)}")
+#     # Convertir la imagen PIL a un array de NumPy
+#     np_image = np.array(pil_style_image)
+#     np_content_image = np.array(pil_content_image)
+#     logging.info(f"Content image type: {type(np_image)}")
+#     logging.info(f"Content image type: {type(np_content_image)}")
 
-    # Guardar las imágenes en el servidor
-    content_path = os.path.join("tmp", "content.jpg")
-    style_path = os.path.join("tmp", "style.jpg")
-    pil_content_image.save(content_path)
-    pil_style_image.save(style_path)
+#     # Guardar las imágenes en el servidor
+#     content_path = os.path.join("tmp", "content.jpg")
+#     style_path = os.path.join("tmp", "style.jpg")
+#     pil_content_image.save(content_path)
+#     pil_style_image.save(style_path)
 
-    # Aplicar style transfer
-    apply_style_transfer(pil_content_image, pil_style_image)
+#     # Aplicar style transfer
+#     apply_style_transfer(pil_content_image, pil_style_image)
 
-    # Devolver la imagen procesada al usuario
-    return response.text("todo ok11")
-    # return await response.file(output_path, filename="output.jpg")
+#     # Devolver la imagen procesada al usuario
+#     return response.text("todo ok11")
+#     # return await response.file(output_path, filename="output.jpg")
 
 
 if __name__ == "__main__":
