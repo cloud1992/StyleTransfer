b41f3247fbef7fb010d374e878a59f2df24c9238
cloud1992 <arturo@halleyscore.com>
2023-05-01 00:18:32 -0300

    server path fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu
-cd style_transfer_app
-sanic server:app &
-#python3 style_transfer_app/server.py > log.out
+python3 style_transfer_app/server.py &
+# cd style_transfer_app
+# sanic server:app &
